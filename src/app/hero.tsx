1bd61a64abee271eaa845d9f168f7621d8463039
milhaud <milhaud1201@gmail.com>
2024-01-15 01:34:44 +0900

    add placeholder

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -12,6 +12,7 @@ function Hero() {
             variant="h1"
             color="blue"
             className="text-3xl !leading-snug"
+            placeholder="category"
           >
             Generative AI
           </Typography>
@@ -19,12 +20,14 @@ function Hero() {
             variant="h1"
             color="blue-gray"
             className="mb-2 max-w-sm text-3xl !leading-snug lg:mb-3 lg:text-3xl"
+            placeholder="title"
           >
             DCV LAB의 아트랩 사업부 Art&Tech 전시 온라인 쇼케이스
           </Typography>
           <Typography
             variant="lead"
             className="mb-6 font-normal !text-gray-500 md:pr-16 xl:pr-28 text-base"
+            placeholder="explanation"
           >
             크리에이터를 지원하기 위해 &apos;Text to Video with Audio&apos;기술을 활용하고 있습니다.
             이 기술을 통해 음악과 비디오를 결합한 작품 활동을 지원하며, 특히 작품의 초기 아이디어 구상 단계에서 크게 도움을 줍니다.
