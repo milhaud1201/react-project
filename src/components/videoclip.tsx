a70cece534ad2a3cffa2ecf272a699da844742f4
milhaud <milhaud1201@gmail.com>
2024-01-14 21:44:18 +0900

    fix typerror

--- src/components/videoclip.tsx
+++ src/components/videoclip.tsx
@@ -31,11 +31,3 @@ class VideoClip extends React.Component {
 }
 
 export default VideoClip;
-
-
-{/* <iframe width="560" height="315"
-src="https://www.youtube.com/embed/PeoVSwjnSjE?si=Idnym4FbEAEmcBe9"
-title="YouTube video player"
-frameborder="0"
-allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-allowfullscreen></iframe> */}
