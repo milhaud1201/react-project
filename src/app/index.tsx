467536525b69bcf37cd494b41015cbaf95caee9a
milhaud <milhaud1201@gmail.com>
2024-01-14 22:05:42 +0900

    rebuild

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,7 +5,6 @@ export * from "./layout";
 export * from "./page";
 export * from "./faq";
 export * from "./other-book-offers";
-export * from "./carousel-features";
 export * from "./get-your-book-from-us";
 
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { Navbar, Footer } from "@/components";
 import Hero from "./hero";
 import TopBookCategories from "./top-book-categories";
 import OtherBookOffers from "./other-book-offers";
-// import CarouselFeatures from "./carousel-features";
 import GetYourBookFromUs from "./get-your-book-from-us";
 import Faq from "./faq";
 
@@ -16,7 +15,6 @@ export default function Campaign() {
       <Hero />
       <TopBookCategories />
       <OtherBookOffers />
-      {/* <CarouselFeatures /> */}
       <GetYourBookFromUs />
       <Faq />
       <Footer />
