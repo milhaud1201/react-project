78eb3331e18db55008b2640352cec38cb0113bff
Jihyeon Park <108461149+milhaud1201@users.noreply.github.com>
2024-03-04 00:37:03 +0900

    fix

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,8 +3,14 @@
 import React from "react";
 import { ThemeProvider } from "@material-tailwind/react";
 
-// Add type definition for ReactNode
-type ReactNode = React.ReactElement | React.Fragment | string | number | null | undefined;
+// Define ReactNode type using conditional type
+type ReactNode =
+  | React.ReactElement
+  | React.Fragment
+  | string
+  | number
+  | null
+  | undefined;
 
 export function Layout({ children }: { children: ReactNode | undefined }) {
   return (
