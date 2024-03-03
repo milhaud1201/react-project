ab8a486bbe7bebb8b25ffa09ed8091481f6ee284
Jihyeon Park <108461149+milhaud1201@users.noreply.github.com>
2024-03-04 01:12:37 +0900

    fix

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,12 +3,8 @@
 import React from "react";
 import { ThemeProvider } from "@material-tailwind/react";
 
-export function Layout({ children }: { children: React.ReactNode | undefined }) {
-  return (
-    <ThemeProvider>
-      {children}
-    </ThemeProvider>
-  );
+export function Layout({ children }: { children: React.ReactNode }) {
+  return <ThemeProvider>{children}</ThemeProvider>;
 }
 
 export default Layout;
